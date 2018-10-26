69d73dff06e6ac10cb0d46133daebdb1db191ac0
Ana Maria Pantoja <anamaria0711@gmail.com>
2018-10-26 08:36:51 -0500

    -Changing Altmetrics to Altmetric. -Adding Open Access and ISI Publications tooltip.

--- app/routes.php
+++ app/routes.php
@@ -12,7 +12,7 @@ $app->get('/[{actionName}]', function ($request, $response, $args) {
     array('name' => 'Innovations',    'action' => 'innovations',  'active' => true ),
     array('name' => 'Partnerships',   'action' => 'partnerships', 'active' => true ),
     array('name' => 'Publications',   'action' => 'publications', 'active' => true ),
-    array('name' => 'Altmetrics',     'action' => 'altmetrics',   'active' => true )
+    array('name' => 'Altmetric',     'action' => 'altmetrics',   'active' => true )
   );
 
   $crps = array(
