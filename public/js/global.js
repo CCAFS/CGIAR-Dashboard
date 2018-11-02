0ee30be2a9a49b9142791d5f0239606b123833fb
Ana Maria Pantoja <anamaria0711@gmail.com>
2018-11-02 11:25:13 -0500

    Disable CRP and Years filters until charts are loaded.

--- public/js/global.js
+++ public/js/global.js
@@ -6,12 +6,25 @@ function init() {
 
 }
 
+// Disable inputs until charts have loaded
+$("input").prop('disabled', true);
+document.getElementById('filtertitley').title = 'Please wait until all charts have loaded';
+document.getElementById('filtertitlec').title = 'Please wait until all charts have loaded';
 
+// Enable inputs after 9 seconds
+function enableinputs() {
+  $("input").prop('disabled', false); 
+  $("#filtertitley").removeAttr('title');
+  $("#filtertitlec").removeAttr('title');
+}
 
-/* Close blue disclaimer in all sections */
+// Wait 9 seconds while charts are loading
+setTimeout(enableinputs, 9000);
+
+// Close blue disclaimer in all sections 
 const showMsg = sessionStorage.getItem('showMsg');
 
-if(showMsg === 'false'){
+if(showMsg == 'false'){
   $('.page-disclaimer').hide();
 }
 
