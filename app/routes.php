5a28ace55f315491e333d81820c16c723f45380f
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-03-13 12:00:25 -0500

    Adding all CRPs and Platforms filter.

--- app/routes.php
+++ app/routes.php
@@ -17,11 +17,18 @@ $app->get('/[{actionName}]', function ($request, $response, $args) {
   );
 
   $crps = array(
-    array('acronym' => 'A4NH',      'name' => 'A4NH - Agriculture for Nutrition and Health'), 
-    array('acronym' => 'CCAFS',     'name' => 'CCAFS - Climate Change, Agriculture and Food Security'), 
+    array('acronym' => 'A4NH',      'name' => 'A4NH - Agriculture for Nutrition and Health'),
+    array('acronym' => 'BigData',      'name' => 'BigData - Big Data in Agriculture'), 
+    array('acronym' => 'CCAFS',     'name' => 'CCAFS - Climate Change, Agriculture and Food Security'),
+    array('acronym' => 'EiB',      'name' => 'EiB - Excellence in Breeding'), 
+    array('acronym' => 'FTA',       'name' => 'FTA - Forests, Trees and Agroforestry'),
+    array('acronym' => 'Genebank', 'name' => 'Genebank'), 
     array('acronym' => 'Livestock', 'name' => 'Livestock'), 
-    array('acronym' => 'PIM',       'name' => 'PIM - Policies, Institutions, and Markets'), 
-    array('acronym' => 'RTB',       'name' => 'RTB - Roots, Tubers and Bananas'), 
+    array('acronym' => 'Maize',       'name' => 'Maize'),
+    array('acronym' => 'PIM',       'name' => 'PIM - Policies, Institutions, and Markets'),
+    array('acronym' => 'Rice',       'name' => 'Rice'),
+    array('acronym' => 'RTB',       'name' => 'RTB - Roots, Tubers and Bananas'),
+    array('acronym' => 'Wheat',       'name' => 'Wheat'),
     array('acronym' => 'WLE',       'name' => 'WLE - Water, Land and Ecosystems') 
   );
 
