af01c14d6da94dd80b1b000460b410314d497871
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-08-26 15:32:58 -0500

    Working in homepage.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -10,7 +10,7 @@ function init() {
         var checkedValues = $.map($checkedInputs, function (e) { return e.value })
     });
 
-    //Countries map
+    //Contribution to SLO bar
     var slodiv = document.getElementById("slo-bar"),
         slourl = appConfig.tableauView + "/HomeDB-SLOBarTop",
         slooptions = {
@@ -29,6 +29,121 @@ function init() {
         };
     slobar = new tableau.Viz(slodiv, slourl, slooptions);
 
+    //Total Innovations
+    var innovationsdiv = document.getElementById("total-innovations"),
+        innovationsurl = appConfig.tableauView + "/HomeDB-InnovCount",
+        innovationsoptions = {
+            hideTabs: true,
+            hideToolbar: true,
+            width: '100%',
+            height: '100%',
+            onFirstInteractive: function () {
+
+                //Hide scrollbars - disable scroll
+                $('#total-innovations iframe').attr("scrolling", "no");
+                $('#total-innovations iframe').css('overflow', 'hidden');
+
+                loaded();
+            }
+        };
+    totalInnovations = new tableau.Viz(innovationsdiv, innovationsurl, innovationsoptions);
+
+    //Total Partnerships
+    var partnershipsdiv = document.getElementById("total-partnerships"),
+        partnershipsurl = appConfig.tableauView + "/HomeDB-PartnershipCount",
+        partnershipsoptions = {
+            hideTabs: true,
+            hideToolbar: true,
+            width: '100%',
+            height: '100%',
+            onFirstInteractive: function () {
+
+                //Hide scrollbars - disable scroll
+                $('#total-partnerships iframe').attr("scrolling", "no");
+                $('#total-partnerships iframe').css('overflow', 'hidden');
+
+                loaded();
+            }
+        };
+    totalPartnerships = new tableau.Viz(partnershipsdiv, partnershipsurl, partnershipsoptions);
+
+
+    //Total Trainees
+    var capDevdiv = document.getElementById("total-capDev"),
+        capDevurl = appConfig.tableauView + "/HomeDB-CapDevCount",
+        capDevoptions = {
+            hideTabs: true,
+            hideToolbar: true,
+            width: '100%',
+            height: '100%',
+            onFirstInteractive: function () {
+
+                //Hide scrollbars - disable scroll
+                $('#total-capDev iframe').attr("scrolling", "no");
+                $('#total-capDev iframe').css('overflow', 'hidden');
+
+                loaded();
+            }
+        };
+    totalCapDev = new tableau.Viz(capDevdiv, capDevurl, capDevoptions);
+
+
+    //Total Publications
+    var publicationsdiv = document.getElementById("total-publications"),
+        publicationsurl = appConfig.tableauView + "/HomeDB-PublicationsCount",
+        publicationsoptions = {
+            hideTabs: true,
+            hideToolbar: true,
+            width: '100%',
+            height: '100%',
+            onFirstInteractive: function () {
+
+                //Hide scrollbars - disable scroll
+                $('#total-publications iframe').attr("scrolling", "no");
+                $('#total-publications iframe').css('overflow', 'hidden');
+
+                loaded();
+            }
+        };
+    totalPublications = new tableau.Viz(publicationsdiv, publicationsurl, publicationsoptions);
+
+    //Total Policies
+    var policiesdiv = document.getElementById("total-policies"),
+        policiesurl = appConfig.tableauView + "/HomeDB-PoliciesCount",
+        policiesoptions = {
+            hideTabs: true,
+            hideToolbar: true,
+            width: '100%',
+            height: '100%',
+            onFirstInteractive: function () {
+
+                //Hide scrollbars - disable scroll
+                $('#total-policies iframe').attr("scrolling", "no");
+                $('#total-policies iframe').css('overflow', 'hidden');
+
+                loaded();
+            }
+        };
+    totalPolicies = new tableau.Viz(policiesdiv, policiesurl, policiesoptions);
+
+    //Total Altmetric
+    var altmetricdiv = document.getElementById("total-altmetric"),
+        altmetricurl = appConfig.tableauView + "/HomeDB-AltmetricCount",
+        altmetricoptions = {
+            hideTabs: true,
+            hideToolbar: true,
+            width: '100%',
+            height: '100%',
+            onFirstInteractive: function () {
+
+                //Hide scrollbars - disable scroll
+                $('#total-altmetric iframe').attr("scrolling", "no");
+                $('#total-altmetric iframe').css('overflow', 'hidden');
+
+                loaded();
+            }
+        };
+    totalAltmetric = new tableau.Viz(altmetricdiv, altmetricurl, altmetricoptions);
 
 }
 
@@ -36,7 +151,7 @@ function init() {
 //Hide "loading" when all charts have loaded
 function loaded() {
     LOADED += 1;
-    if (LOADED == 1) {
-      $("#loadingModal").modal('hide');
+    if (LOADED == 7) {
+        $("#loadingModal").modal('hide');
     }
-  }
+}
