ae37513b3b2b935992dba03e0ceb35f72ffa122b
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-09-26 11:25:10 -0500

    Working in Partnerships 2018 filters.

--- public/js/partnerships-2018.js
+++ public/js/partnerships-2018.js
@@ -2,8 +2,19 @@ var sheetsArray = [];
 var loadedCount = 0;
 
 //Filters
+var FILTER_MAP = "Country Name";
+var FILTER_AREA = "Partnership Main Areas";
+var FILTER_TYPE = "Type";
+var FILTER_TOP = "Partner Name";
 
 //Sheets
+var MAP_SHEET = "3.6 SH Partnership 2018 - Map";
+var AREA_SHEET = "3.2 SH Partner 2018 -Pie by Main Area";
+var TYPE_SHEET = "3.3 SH Partners 2018 - Bar by Type";
+var TOP_SHEET = "3.4 SH Partners 2018 - Top Partners";
+var LIST_SHEET = "3.5 SH Partners 2018 - Detail";
+
+
 
 
 $(document).ready(init);
@@ -18,15 +29,18 @@ function init() {
   ];
 
   vizInitialited = [];
-  $.each(vizDataArray, function(i, data){
-    vizInitialited.push(createTableauViz( data.elementID, data.view, [ onSelectWorkSheet ]))
+  $.each(vizDataArray, function (i, data) {
+    vizInitialited.push(createTableauViz(data.elementID, data.view, [onSelectWorkSheet]))
   });
 }
 
-function loadSheets(){
-  sheetsArray = [
-
-  ];
+function loadSheets() {
+  $.each(vizInitialited, function (i, viz) {
+    var sheetsList = viz.getWorkbook().getActiveSheet().getWorksheets();
+    $.each(sheetsList, function (i, s) {
+      sheetsArray.push(s);
+    });
+  });
 }
 
 //Hide "loading" when all charts have loaded
@@ -40,13 +54,23 @@ function loaded() {
   }
 }
 
-function onSelectWorkSheet(mEvent){
+function onSelectWorkSheet(mEvent) {
   var selectedSheet = mEvent.getWorksheet();
   var selectedSheetName = selectedSheet.getName();
-  return mEvent.getMarksAsync().then(function(marks){
-    var filterName, tagName, $tag, clearFunction;
-    switch(selectedSheetName) {
-
+  return mEvent.getMarksAsync().then(function (marks) {
+    switch (selectedSheetName) {
+      case MAP_SHEET:
+        setFilterWorksheet(marks, FILTER_MAP, sheetsArray, selectedSheet, selectedSheetName, 'Headquarter Location');
+        break;
+      case AREA_SHEET:
+        setFilterWorksheet(marks, FILTER_AREA, sheetsArray, selectedSheet, selectedSheetName, 'Main Area');
+        break;
+      case TYPE_SHEET:
+        setFilterWorksheet(marks, FILTER_TYPE, sheetsArray, selectedSheet, selectedSheetName, 'Type');
+        break;
+      case TOP_SHEET:
+        setFilterWorksheet(marks, FILTER_TOP, sheetsArray, selectedSheet, selectedSheetName, 'Partner');
+        break;
     }
   });
 }
