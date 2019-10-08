48e62ba067b421d2a9d1378f54b692f68b71698e
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-10-08 11:44:25 -0500

    Adding indicators icons and splitting the numbers in two dashboards.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -9,7 +9,8 @@ function init() {
     { elementID: 'slo-bar', view: 'HomeDB-SLOBarTop' },
     { elementID: 'oicr-chart', view: 'HomeDB-OICRBarchartyear' },
     { elementID: 'milestones', view: 'HomeDB-Milestonesdonut' },
-    { elementID: 'reporting-indicators', view: 'HomeDBIndicatorNumbers' },
+    { elementID: 'indicators-top', view: 'HomeDBIndicatorNumbers-Top' },
+    { elementID: 'indicators-bottom', view: 'HomeDBIndicatorNumbers-Down' },
     { elementID: 'insights-chart', view: 'HomeDB-Insights'}
   ];
 
