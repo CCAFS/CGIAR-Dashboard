bfbfff1b178179e202a2da72025ac846207b1715
Yecksin <yecksin@gmail.com>
2021-02-24 08:40:22 -0500

    Change links (CapDev, Innovations, OICRs)

--- public/js/oicr.js
+++ public/js/oicr.js
@@ -41,7 +41,7 @@ function init() {
 
   vizInitialited = [];
   $.each(vizDataArray, function (i, data) {
-    vizInitialited.push(createTableauViz(data.elementID, data.view, [onSelectWorkSheet]))
+    vizInitialited.push(createTableauVizNew(data.elementID, data.view, [onSelectWorkSheet]))
   });
 
 }
